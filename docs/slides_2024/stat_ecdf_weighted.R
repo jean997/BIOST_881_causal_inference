22614abf6384cb768bde5cdf76746676ded82ab6
Jean Morrison <morrison.jeanv@gmail.com>
2024-02-19 14:39:30 -0500

    fix figures

--- docs/slides_2024/stat_ecdf_weighted.R
+++ docs/slides_2024/stat_ecdf_weighted.R
@@ -82,7 +82,7 @@ StatEcdf <- ggproto("StatEcdf", Stat,
                       if (pad) {
                         x <- c(-Inf, x, Inf)
                       }
-                      y <- spatstat.geom::ewcdf(data$x, weights=data$weight/sum(data$weight))(x)
+                      y <- ewcdf(data$x, weights=data$weight/sum(data$weight))(x)
 
                       data.frame(x = x, y = y)
                     },
